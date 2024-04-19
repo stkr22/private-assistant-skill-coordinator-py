5a92cfc75f7363c0fe747b8a0c56a1f364e26ab2
stkr22 <checkout@github.com>
2024-04-19 15:22:29 +0000

    fixing broken registry timestamp; releasing new version 0.1.6;

--- src/private_assistant_skill_coordinator/messages.py
+++ src/private_assistant_skill_coordinator/messages.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
 from private_assistant_commons import messages
+from pydantic import Field
 
 
 class SkillRegistration(messages.SkillRegistration):
-    registered_at: datetime = datetime.now()
+    registered_at: datetime = Field(default_factory=datetime.now)
